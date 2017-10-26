cabd0d45fad3045f54881f1a2305548b3534c128
Tom Slinger <t.slinger@outlook.com>
2017-10-26 13:07:19 +0100

    basic post a tweet

---
+++ app.js
@@ -0,0 +1,13 @@
+var TwitterPackage = require('twitter');
+var apiKeys = require('./config.js');
+
+var Twitter = new TwitterPackage(apiKeys);
+
+// Post a Tweet!
+Twitter.post('statuses/update', {status: 'I like Football! #TweetedByBot'}, function(error, tweet, response){
+    if(error){
+        console.log(error);
+    }
+    console.log(tweet); // Console.log the tweet body
+    // console.log(response); // Console.log the raw response object
+})
