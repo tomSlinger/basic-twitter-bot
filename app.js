1c4c9b8158739dbbe362cbb0fddfb6743682757e
Tom Slinger <t.slinger@outlook.com>
2017-10-26 13:13:15 +0100

    added notes section

--- app.js
+++ app.js
@@ -11,4 +11,12 @@ Twitter.post('statuses/update', {status: 'I like Football! #TweetedByBot'}, func
     }
     console.log(tweet); // Console.log the tweet body
     // console.log(response); // Console.log the raw response object
-})
+});
+
+/* Notes for above:
+1) Twitter.post is a call to the post function in the Twitter object
+2) 'statuses/update' means want to post a status update (aka a tweet)
+3) {status: 'I love Football! #TweetedByBot'} is a JS object setting the actual status body message
+4) We finish with a function that can catch any errors and show us other information if needed
+
+*/
